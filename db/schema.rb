b1610e124c92ac266deef1c2315819b0d1cdffd3
Adil Omary <adilomary@hotmail.fr>
2020-04-22 15:25:34 +0200

    Fix on Character model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_165111) do
+ActiveRecord::Schema.define(version: 2020_04_22_132010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "constitution"
+    t.integer "dexterity"
+    t.integer "strenght"
+    t.integer "gunfight"
+    t.integer "misc"
+    t.string "weapons"
+    t.string "equipment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
