e3310d3daf07581f38ef8c1ff75e765905355795
Adil Omary <adilomary@hotmail.fr>
2020-04-22 18:51:55 +0200

    add validations to User model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,7 @@
 class Character < ApplicationRecord
+  belongs_to :user
+  belongs_to :game
+
+  validates :name, :constitution, :dexterity, :strenght, :gunfight, :misc, :weapons, :equipment, presence: true
+  validates :constitution, :dexterity, :strenght, :gunfight, :misc, numericality: { only_integer: true }
 end
